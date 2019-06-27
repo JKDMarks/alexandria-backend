804ea3369cacf4aba1e38a2bf87061e050bc7646
Jeffrey Marks <JKDMarks@gmail.com>
2019-06-27 16:57:08 -0400

    Real commit. :P Begin create deck. Create DeckCard doesn't yet work

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -3,4 +3,31 @@ class DecksController < ApplicationController
     @decks = Deck.all
     render json: @decks
   end
+
+  def create
+    @deck = Deck.new(deck_params)
+
+    if @deck.valid?
+      @deck.save
+
+      params.permit(:cards).each do |card|
+        DeckCard.create(
+          deck_id: @deck.id,
+          user_id: params.permit(:user_id),
+          quantity: card["quantity"]
+        )
+      end
+
+      render json: @deck
+    else
+      render json: { error: "Something went wrong" }
+    end
+  end
+
+
+  private
+
+  def deck_params
+    params.permit(:user_id, :name, :format, :image)
+  end
 end
