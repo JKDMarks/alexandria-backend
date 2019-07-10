a118b75ed2367b893dca42186a9ef3764822a50b
Jeffrey Marks <JKDMarks@gmail.com>
2019-07-10 17:25:07 -0400

    Update missing card images

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -18,13 +18,31 @@ class CardsController < ApplicationController
     render json: cards_by_format
   end
 
-  # def update_image
-  #   img = params[:imageUrl]
-  #   img = img[0..img.index("?")-1]
-  #   card = Card.find_by("image_uris ~* ?", img)
-  #
-  #   updated_card = JSON.parse(open("https://api.scryfall.com/cards/#{card.scryfall_id}").read)
-  #
-  #   card.update(image_uris: updated_card["image_uris"])
-  # end
+  def update_image
+    card_id = params[:cardId]
+    if card_id
+      card = Card.find(card_id)
+      user = nil
+    else
+      image = params[:imageURL]
+      image = image[0..image.index("?")-1]
+      user = User.find_by("image ~* ?", image)
+      card = Card.find_by("image_uris ~* ?", image)
+    end
+
+    if card
+      updated_card = JSON.parse(open("https://api.scryfall.com/cards/#{card.scryfall_id}").read)
+
+      card.update(image_uris: updated_card["image_uris"])
+
+      if user
+        user.update(image: card.image_uris["art_crop"])
+      end
+    else
+      if user
+        user.update(image: Card.all.sample.image_uris["art_crop"])
+      end
+    end
+
+  end
 end
